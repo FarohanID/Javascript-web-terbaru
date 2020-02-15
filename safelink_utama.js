7af0172dab882fedbe291305c4cfbbfef0b2a11a
Farhan <ahmad.farohan@gmail.com>
2020-02-15 21:07:47 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="download1906.html"}
-if(x=="2"){xxx="download1907.html"}
-if(x=="3"){xxx="download1908.html"}
-if(x=="4"){xxx="download1909.html"}a_to_ve[i].href="https://www.safelinks.icu/2018/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="direct-safelink.html"}
+if(x=="2"){xxx="direct-safelink.html"}
+if(x=="3"){xxx="direct-safelink.html"}
+if(x=="4"){xxx="direct-safelink.html"}a_to_ve[i].href="https://direct-safelink-indonesia.blogspot.com/2017/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
